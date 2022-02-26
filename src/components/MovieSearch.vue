8914a397d9dfc45afb02ea9bdcd10067cf99163e
pcsontos <peter_csontos@hotmail.com>
2022-02-26 17:32:47 +0100

    Update MovieSearch, search with enter key

--- src/components/MovieSearch.vue
+++ src/components/MovieSearch.vue
@@ -10,20 +10,19 @@ const search = () => {
 
 </script>
 <template>
-  <el-row :gutter="20">
-    <el-col :span="8">
-      <div class="mt-4">
-        <el-input
-            v-model="searchFor"
-            placeholder="Movie Title (e.g. The Godfather)"
-            class="input-with-select"
-            clearable
-        >
-          <template #append>
-            <el-button :icon="Search" @click="search"></el-button>
-          </template>
-        </el-input>
-      </div>
+  <el-row>
+    <el-col :span="16">
+      <el-input
+          v-model="searchFor"
+          placeholder="Movie Title (e.g. The Godfather)"
+          class="input-with-select"
+          clearable
+          @keyup.enter="search"
+      >
+        <template #append>
+          <el-button :icon="Search" @click="search"></el-button>
+        </template>
+      </el-input>
     </el-col>
   </el-row>
 
