e0f6777f2aaae2757b4b3088f22eda11720fbd11
pcsontos <peter_csontos@hotmail.com>
2022-02-26 15:54:08 +0100

    MovieSearch component created

---
+++ src/components/MovieSearch.vue
@@ -0,0 +1,30 @@
+<script setup>
+import {ref} from 'vue'
+import {Search} from '@element-plus/icons-vue'
+
+const searchFor = ref('')
+
+const search = () => {
+  console.log(`searching... ${searchFor.value}`)
+}
+
+</script>
+<template>
+  <el-row :gutter="20">
+    <el-col :span="8">
+      <div class="mt-4">
+        <el-input
+            v-model="searchFor"
+            placeholder="Movie Title (e.g. The Godfather)"
+            class="input-with-select"
+            clearable
+        >
+          <template #append>
+            <el-button :icon="Search" @click="search"></el-button>
+          </template>
+        </el-input>
+      </div>
+    </el-col>
+  </el-row>
+
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,10 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MovieSearch />
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+<script setup>
+import MovieSearch from '@/components/MovieSearch.vue';
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld,
-  },
-};
 </script>
